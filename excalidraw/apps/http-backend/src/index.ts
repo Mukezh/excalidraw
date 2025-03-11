614d29a124b9de4d18b97518de95e6d5983876e9
Mukezh <mukesh.offcl@gmail.com>
2025-03-11 07:25:35 +0530

    mar 11

--- excalidraw/apps/http-backend/src/index.ts
+++ excalidraw/apps/http-backend/src/index.ts
@@ -2,5 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/signup', async(req,res) => {
+    const {username, password} = req.body;
+
+    const data = User.create({
+        username: username,
+        password: password
+    })
+
+} )
 
 app.listen(3001);
