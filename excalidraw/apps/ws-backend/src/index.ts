e6410581548a8096b0cd8232c2c0d79acabff1b2
Mukezh <mukesh.offcl@gmail.com>
2025-04-01 13:25:06 +0530

    restart

--- excalidraw/apps/ws-backend/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { WebSocket, WebSocketServer } from "ws";
-
-const ws = new WebSocketServer({port : 8080})
-ws.on('connection', function connection(ws) {
-    ws.send('you are connected');
-})
